9fe076cc91b6b9fb48223c77512a8f9be6c5e697
Anidog <102453582+AnidogOG@users.noreply.github.com>
2026-03-07 11:32:36 -0700

    basic setup, first song, and some stage junk

--- source/options/NoteOffsetState.hx
+++ source/options/NoteOffsetState.hx
@@ -406,7 +406,14 @@ class NoteOffsetState extends MusicBeatState
 
 			persistentUpdate = false;
 			MusicBeatState.switchState(new options.OptionsState());
-			FlxG.sound.playMusic(Paths.music('freakyMenu'));
+			if(OptionsState.onPlayState)
+			{
+				if(ClientPrefs.data.pauseMusic != 'None')
+					FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.data.pauseMusic)));
+				else
+					FlxG.sound.music.volume = 0;
+			}
+			else FlxG.sound.playMusic(Paths.music('freakyMenu'));
 			FlxG.mouse.visible = false;
 		}
 
